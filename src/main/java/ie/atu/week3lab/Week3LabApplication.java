3446444ef334dd75682b3085c4e8b21f201cc6a8
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2025-10-01 13:24:22 +0100

    Start code for week 3 lab

---
+++ src/main/java/ie/atu/week3lab/Week3LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3lab/Week3LabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
